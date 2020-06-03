ab4d6e9ff8587ed0bf05e7c19e1cd8ef56f54620
Tiago Mota Barbosa <tiagomb.privado@gmail.com>
2020-06-03 14:52:00 -0300

    Aula 02 - Finalizada

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -54,7 +54,7 @@ class PointsController {
     const trx = await knex.transaction(); // Faz rollback se alguma query não rodar!
   
     const point = {
-      image: 'image-fake',
+      image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
       name,
       email,
       whatsapp,
