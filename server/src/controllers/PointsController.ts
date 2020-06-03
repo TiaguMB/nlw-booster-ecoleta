3cd2c4ddcebbbc99b19a84f614f799ca29555c7b
Tiago Mota Barbosa <tiagomb.privado@gmail.com>
2020-06-03 12:39:25 -0300

    Aula 02 - Criado rota /points/:id (específico)

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -2,6 +2,24 @@ import { Request, Response } from 'express';
 import knex from '../database/connection';
 
 class PointsController {
+  async show(request: Request, response: Response) {
+    // const id = request.params.id;
+    const { id } = request.params;
+
+    const point = await knex('points').where('id', id).first();
+
+    if (!point) {
+      return response.status(400).json({ message: 'Point not found.' });
+    }
+
+    const items = await knex('items')
+      .join('point_items', 'items.id', '=', 'point_items.item_id')
+      .where('point_items.point_id', id)
+      .select('items.title');
+
+    return response.json({ point, items });
+  };
+
   async create(request: Request, response: Response) {
     const {
       name,
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -10,7 +10,9 @@ const pointsController = new PointsController;
 const itemsController = new ItemsController;
 
 routes.get('/items', itemsController.index);
+
 routes.post('/points', pointsController.create);
+routes.get('/points/:id', pointsController.show);
 
 export default routes;
 
