e64de4e9566f0a8136eed71e146f6ca1c9ca3325
Tiago Mota Barbosa <tiagomb.privado@gmail.com>
2020-06-03 10:11:31 -0300

    Aula 02 - Adicionado listagem de imagens (/items)

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,19 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-  return response.json({
-    message: 'Hello World'
+routes.get('/items', async (request, response) => {
+  const items = await knex('items').select('*');
+  
+  const serializedItems = items.map(item => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    };
   });
+  
+  return response.json({ serializedItems });
 });
 
 export default routes;
