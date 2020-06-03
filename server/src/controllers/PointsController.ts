affad964d20dbca66f3cb2085748a1c973aca04a
Tiago Mota Barbosa <tiagomb.privado@gmail.com>
2020-06-03 13:29:10 -0300

    Aula 02 - Criado rota /points filtrado

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -2,6 +2,25 @@ import { Request, Response } from 'express';
 import knex from '../database/connection';
 
 class PointsController {
+  async index(request: Request, response: Response) {
+    // Cidade, uf, items
+    const { city, uf, items } = request.query;
+
+    const parsedItems = String(items)
+      .split(',')
+      .map(item => Number(item.trim()));
+
+    const points = await knex('points')
+      .join('point_items', 'points.id', '=', 'point_items.point_id')
+      .whereIn('point_items.item_id', parsedItems)
+      .where('city', String(city))
+      .where('uf', String(uf))
+      .distinct()
+      .select('points.*');
+
+    return response.json(points);
+  }
+
   async show(request: Request, response: Response) {
     // const id = request.params.id;
     const { id } = request.params;
