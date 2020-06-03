54db8e8354304d4617d78effa4ddeb2abafeab9d
Tiago Mota Barbosa <tiagomb.privado@gmail.com>
2020-06-03 11:01:36 -0300

    Aula 02 - Criado rota /points

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -8,12 +8,53 @@ routes.get('/items', async (request, response) => {
   
   const serializedItems = items.map(item => {
     return {
+      id: item.id,
       title: item.title,
       image_url: `http://localhost:3333/uploads/${item.image}`,
     };
   });
   
   return response.json({ serializedItems });
+});
+
+
+routes.post('/points', async (request, response) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items
+  } = request.body;
+
+  const trx = await knex.transaction(); // Faz rollback se alguma query não rodar!
+
+  const insertedIds = await trx('points').insert({
+    image: 'image-fake',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf
+  });
+
+  const point_id = insertedIds[0];
+
+  const pointItems = items.map((item_id: number) => {
+    return {
+      item_id,
+      point_id,
+    };
+  });
+  
+  await trx('point_items').insert(pointItems);
+
+  return response.json({ success: true });
 });
 
 export default routes;
