5f7a8a063ca230d63a8ae99f9fa6ba11e15a3e08
Tiago Mota Barbosa <tiagomb.privado@gmail.com>
2020-06-03 11:52:35 -0300

    Aula 02 - Desacoplamento (patterns)

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,60 +1,20 @@
 import express from 'express';
-import knex from './database/connection';
+
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
+
+// index, show, create/store, update, delete/destroy <-- Padrão de Controllers
 
 const routes = express.Router();
+const pointsController = new PointsController;
+const itemsController = new ItemsController;
 
-routes.get('/items', async (request, response) => {
-  const items = await knex('items').select('*');
-  
-  const serializedItems = items.map(item => {
-    return {
-      id: item.id,
-      title: item.title,
-      image_url: `http://localhost:3333/uploads/${item.image}`,
-    };
-  });
-  
-  return response.json({ serializedItems });
-});
-
-
-routes.post('/points', async (request, response) => {
-  const {
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf,
-    items
-  } = request.body;
-
-  const trx = await knex.transaction(); // Faz rollback se alguma query não rodar!
-
-  const insertedIds = await trx('points').insert({
-    image: 'image-fake',
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf
-  });
-
-  const point_id = insertedIds[0];
-
-  const pointItems = items.map((item_id: number) => {
-    return {
-      item_id,
-      point_id,
-    };
-  });
-  
-  await trx('point_items').insert(pointItems);
-
-  return response.json({ success: true });
-});
+routes.get('/items', itemsController.index);
+routes.post('/points', pointsController.create);
 
 export default routes;
+
+
+// Outras possíveis lógicas para abstração:
+// Service Pattern
+// Repository Patten (Data Mapper)
