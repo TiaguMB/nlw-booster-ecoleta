e33627fbc541ed5adba28950a268d4e9692c5c38
Tiago Mota Barbosa <tiagomb.privado@gmail.com>
2020-06-05 02:45:08 -0300

    Aula 03 - Feito configuração da navegação

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import Routes from './routes';
+
 import Home from './pages/Home';
 
 function App() {
   return (
-    <Home />
+    <Routes />
   );
 }
 
